89895ecbe7989251eebc1c0846489aa3e1ca0a5a
peter <niqixian@gmail.com>
2024-02-06 18:14:19 -0600

    Fix the mistake that I catch

--- guess_number.py
+++ guess_number.py
@@ -3,11 +3,12 @@ while True:
     print("I'm thinking of a number...")
     guess = (input("What number am I thinking of? "))
 
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+    guess = int(guess)
     if guess == number:
         print("Congratulations! You guessed the right number.")
-        break
-    elif guess == 'q':
-        print(f"Sorry! The number was {number}.")
         break
     else:
         print(f"Sorry! Please Try Again")
