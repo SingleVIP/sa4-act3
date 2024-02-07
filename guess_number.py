f3c32e955e7ac22eb731a872dec42e663f044c15
peter <niqixian@gmail.com>
2024-02-06 18:10:34 -0600

    Modify the code to tell higher or lower

--- guess_number.py
+++ guess_number.py
@@ -3,11 +3,17 @@ while True:
     print("I'm thinking of a number...")
     guess = (input("What number am I thinking of? "))
 
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+    guess = int(guess)
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
-    elif guess == 'q':
-        print(f"Sorry! The number was {number}.")
-        break
+    elif guess > number:
+        print('Sorry! The number was lower ')
+    elif guess < number:
+        print('Sorry! The number was higher ')
+    
     else:
         print(f"Sorry! Please Try Again")
