488977adf0778842a638d1cbc01bd386d0605842
peter <niqixian@gmail.com>
2024-02-06 18:32:09 -0600

    Modify the code to show chance

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,8 @@
 number = 10
-while True:
+chances = 5
+while chances > 0:
     print("I'm thinking of a number...")
-    guess = (input("What number am I thinking of? "))
+    guess = (input(f"What number am I thinking of? You have {chances} chances left: "))
 
     if guess == 'q':
         print(f"Sorry! The number was {number}.")
@@ -10,5 +11,12 @@ while True:
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif guess > number:
+        print('Sorry! The number was lower ')
+    elif guess < number:
+        print('Sorry! The number was higher ')
     else:
         print(f"Sorry! Please Try Again")
+    chances -= 1
+else:
+    print(f"Sorry, you've run out of guesses. The number was {number}.")
